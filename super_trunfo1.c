a5d353c5ec70efb4e655a3c78e42f5c512f936c8
pgrm07 <enzo06112007@gmail.com>
2025-09-23 01:15:00 +0000

    Carta 2

--- super_trunfo1.c
+++ super_trunfo1.c
@@ -3,4 +3,5 @@
 int main(){
     printf("Desafio Cartas Super Trunfo!\n");
     printf("Carta 1: Estado: Ceara Código: A01 Nome da Cidade: Fortaleza População: 12325000 Área: 1521.11 km² PIB: 699.28 bilhões de reais Número de Pontos Turísticos: 50"\n);
+    printf("Carta 2: Estado: Amazonas Código: B02 Nome da Cidade: Manaus População: 6748000 Área: 1200.25 km² PIB: 300.50 bilhões de reais Número de Pontos Turísticos: 30"\n);
 }
