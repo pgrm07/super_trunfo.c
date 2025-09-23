3e6391e8dcba10a7d3ef9eb89dc6bc06f9ecda27
pgrm07 <enzo06112007@gmail.com>
2025-09-23 01:10:10 +0000

    Carta 1

--- super_trunfo1.c
+++ super_trunfo1.c
@@ -2,5 +2,5 @@
 
 int main(){
     printf("Desafio Cartas Super Trunfo!\n");
-    printf("Novo commit!"\n);
+    printf("Carta 1: Estado: Ceara Código: A01 Nome da Cidade: Fortaleza População: 12325000 Área: 1521.11 km² PIB: 699.28 bilhões de reais Número de Pontos Turísticos: 50"\n);
 }
